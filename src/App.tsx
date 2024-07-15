d2b9f9a58fc091533aab83a0f89766eef6572d74
trinantaasembiring <kurniawan.rizki.2305356@students.um.ac.id>
2024-07-16 00:53:16 +0700

    Adding new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useSpring, animated, SpringValues, useTransition } from '@react-spring/web';
-import { useInView } from 'react-intersection-observer';
 import dermaWashLogo from './assets/img/logo-header.png';
 import bgHeroSection1 from './assets/img/hero-section.png';
 import bgHeroSection2 from './assets/img/hero-section.jpg';
@@ -12,30 +11,41 @@ import secondAdvantage from './assets/img/48.png'
 import thirdAdvantage from './assets/img/49.png'
 import fourthAdvantage from './assets/img/50.png'
 import fifthAdvantage from './assets/img/51.png'
-
+import beforeAbout from './assets/img/bgBeforeAbout.png'
+import rightAdvantage from './assets/img/advantage-right.png'
+import citrusLg from './assets/img/dermawash-product/citrus-lg.png'
+import sakuraLg from './assets/img/dermawash-product/sakura-lg.png'
+import lavenderLg from './assets/img/dermawash-product/lavender-lg.png'
+import citrusSm from './assets/img/dermawash-product/citrus-sm.png'
+import sakuraSm from './assets/img/dermawash-product/sakura-sm.png'
+import lavenderSm from './assets/img/dermawash-product/sakura-sm.png'
+import AOS from 'aos'
+import 'aos/dist/aos.css';
 interface AppProps {
   duration?: number;
 }
-
 const App: React.FC<AppProps> = ({ duration = 1000 }) => {
+  useEffect(() => {
+    AOS.init()
+    },[])
   const [index, setIndex] = useState(0);
   const [idxAboutProduct, setIdxAboutProduct] = useState(0);
   const backgrounds = [bgHeroSection1, bgHeroSection2];
   const productAbout = [
     {
       img:citrusProduct,
-      shadow:'rgba(83, 115, 67, 0.4) -5px 5px, rgba(83, 115, 67, 0.3) -10px 10px, rgba(83, 115, 67, 0.2) -15px 15px, rgba(83, 115, 67, 0.1) -20px 20px, rgba(83, 115, 67, 0.05) -25px 25px'
+      shadow:'rgba(83, 115, 67, 0.4) 0px 5px, rgba(83, 115, 67, 0.3) 0px 10px, rgba(83, 115, 67, 0.2) 0px 15px, rgba(83, 115, 67, 0.1) 0px 20px, rgba(83, 115, 67, 0.05) 0px 25px'
     },
     {
       img:lavenderProduct,
-      shadow:'rgba(164, 90, 194, 0.4) -5px 5px, rgba(164, 90, 194, 0.3) -10px 10px, rgba(164, 90, 194, 0.2) -15px 15px, rgba(164, 90, 194, 0.1) -20px 20px, rgba(164, 90, 194, 0.05) -25px 25px'
+      shadow:'rgba(164, 90, 194, 0.4) 0px 5px, rgba(164, 90, 194, 0.3) 0px 10px, rgba(164, 90, 194, 0.2) 0px 15px, rgba(164, 90, 194, 0.1) 0px 20px, rgba(164, 90, 194, 0.05) 0px 25px'
     },
     {
       img:sakuraProduct,
-      shadow:'rgba(204, 164, 168, 0.4) -5px 5px, rgba(204, 164, 168, 0.3) -10px 10px, rgba(204, 164, 168, 0.2) -15px 15px, rgba(204, 164, 168, 0.1) -20px 20px, rgba(204, 164, 168, 0.05) -25px 25px'
+      shadow:'rgba(204, 164, 168, 0.4) 0px 5px, rgba(204, 164, 168, 0.3) 0px 10px, rgba(204, 164, 168, 0.2) 0px 15px, rgba(204, 164, 168, 0.1) 0px 20px, rgba(204, 164, 168, 0.05) 0px 25px'
     }
   ];
-  const colorAboutDescription = ['#537343', '#A45AC2', '#CCA4A8'];
+  const colorAboutDescription = ['black','black','black'];
 
   const transitions = useTransition(index, {
     from: { opacity: 0 },
@@ -75,7 +85,6 @@ const App: React.FC<AppProps> = ({ duration = 1000 }) => {
   }, [productAbout.length]);
 
   const slideIn = (duration: number) => useSlideInAnimation(duration);
-  // const slideInFromTop = (duration: number) => useSlideInFromTopAnimation(duration);
 
   return (
     <div className='container'>
@@ -120,18 +129,21 @@ const App: React.FC<AppProps> = ({ duration = 1000 }) => {
         </div>
       </div>
 
-      <div id='about-section' className='about-section'>
+      <div className='before-about' style={{display:"flex",justifyContent:"center",alignItems:"center",background:`url(${beforeAbout})`,marginTop:"100px", marginBottom:"100px",height:"550px",backgroundSize:"cover"}}>
+        <h1   style={{ color:"#537343", fontSize:"130px"}}>Dermawash</h1>
+      </div>
+      <div data-aos='zoom-in-down'  data-aos-duration="900" id='about-section' className='about-section'>
         <div className='content-about'>
           <div className='product-image-container' style={{ position: "relative", width: "400px" }}>
             {transitionAboutProducts((style, i) => (
               <animated.img className="img-about-product" style={{ ...style, position: "absolute",boxShadow:`${productAbout[i].shadow}` }} src={productAbout[i].img} width={400} alt="" />
             ))}
           </div>
-          <div className='description-about'>
+          <div className='description-about' style={{backgroundColor:"#D6E6CC", padding:"35px"}}>
             <div className='text-description'>
               {transitionDescriptionColor((style, i) => (
                 <animated.div style={{...style, position:"relative"}} key={i}>
-                  <h3 style={{position:"absolute", fontSize:"50px", letterSpacing:"4px"}}>About Dermawash</h3>
+                  <h3 style={{position:"absolute", fontSize:"50px"}}>About Dermawash</h3>
                 </animated.div>
               ))}
               <br />
@@ -141,47 +153,312 @@ const App: React.FC<AppProps> = ({ duration = 1000 }) => {
         </div>
       </div>
 
-      <div id="advantage-section" className="advantage-section">
-        <div className='advantage' style={{paddingTop:"100px",paddingBottom:"100px"}}>
-          <center className='title-advantage'>
-          <p><b><span id='dermawash-span-about'>DERMAWASH</span> Excellence</b></p>
-          </center>
-          <div style={{display:"flex", justifyContent:"center",gap:"100px", marginTop:"50px"}}>
-            <AnimatedCardAdv imgSrc={firstAdvantage} text="Alcohol and SLS free Efektif" />
-            <AnimatedCardAdv imgSrc={secondAdvantage} text="Antibacterial formula" />
-            <AnimatedCardAdv imgSrc={fifthAdvantage} text="Melembabkan kulit serta menenangkan kulit" />
-          </div>
-          <div style={{display:"flex", justifyContent:"center",gap:"100px",marginTop:"40px"}}>
-            <AnimatedCardAdv imgSrc={fourthAdvantage} text="Anti-irritrant agent, sehingga cocok untuk kulit sensitive" />
-            <AnimatedCardAdv imgSrc={thirdAdvantage} text="Vitamin E untuk melembabkan kulit serta menenangkan kulit" />
+      <div 
+      id="advantage-section" 
+      className="advantage-section" 
+      style={{
+        display:"flex", 
+        justifyContent:"center", 
+        alignItems:"center",
+        marginBottom:"70px"
+        }}>
+        <div
+        style={{display:"flex"}}
+        >
+              <div 
+              className="advantage-item">
+                <div
+                style={{display:"flex",gap:"20px"}}
+                >
+                  <figure data-aos='fade-down'  data-aos-duration="900">
+                    <center>
+                    <img  src={firstAdvantage} alt="" width={200} />
+                    <p style={{fontSize:"25px"}}>Alcohol & SLS <br />Free</p>
+                    </center>
+                  </figure>
+                  <figure data-aos='fade-down'  data-aos-duration="900">
+                    <center>
+                    <img src={secondAdvantage} alt="" width={200} />
+                    <p style={{fontSize:"25px"}}>Antibacterial <br /> formula</p>
+                    </center>
+                  </figure>
+                  <figure data-aos='fade-down'  data-aos-duration="900"> 
+                    <center>
+                    <img src={thirdAdvantage} alt="" width={200} />
+                    <p style={{fontSize:"25px"}}>Vitamin E</p>
+                    </center>
+                  </figure>
+                </div>
+                <div
+                style={{display:"flex",gap:"20px", justifyContent:"center"}}
+                >
+                    <figure data-aos='fade-down'  data-aos-duration="900">
+                    <center>
+                    <img src={fourthAdvantage} alt="" width={200} />
+                    <p style={{fontSize:"25px"}}>Melembabkan</p>
+                    </center>
+                  </figure>
+                  <figure data-aos='fade-down'  data-aos-duration="900"> 
+                    <center>
+                    <img src={fifthAdvantage} alt="" width={200} />
+                    <p style={{fontSize:"25px"}}>Efektif terhadap <br/>kurap</p>
+                    </center>
+                  </figure>
+                </div>
+              </div>
+              <div data-aos='fade-down'  data-aos-duration="900" className="advetage-right">
+                <img src={rightAdvantage} alt="right-image-advantage" width={400} />
+              </div>
+        </div>
+      </div>
+
+      <div 
+       data-aos='fade-down'  data-aos-duration="900"
+      style={{
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        paddingTop:"100px",
+        paddingBottom:"100px"
+
+      }}
+      className="product-list-section" id="product-list-section">
+        <div>
+          <div 
+          className='product-list'
+          style={{
+            display:"flex",
+            justifyContent:"space-around",
+            overflowX:"scroll",
+            gap:"20px",
+            width:"1100px",
+            borderRadius:"20px",
+            paddingBottom:"10px"
+          }}
+          >
+            <div
+            style={{
+              backgroundColor:"#B8D5A9"
+            }}
+            className='product-card'
+            >     
+              <figure>
+                <center>
+                  <img src={citrusLg} alt="jeruk-large" />
+                </center>
+                <figcaption
+                style={{
+                  marginLeft:"20px",
+                }}
+                >
+                    <div
+                    style={{
+                      transform:"translateY(200px)"
+                    }}
+                    >
+                    <h2 style={{
+                      color:"white"
+                    }}>Jeruk</h2>
+                      <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>Rp. 55.0000</h3>
+                     <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>250 ml</h3>
+                    </div>
+                   
+                </figcaption>
+              </figure>
+            </div>
+            <div
+            style={{
+              backgroundColor:"#DEB1B8"
+            }}
+            className='product-card'
+            >     
+              <figure>
+                <center>
+                  <img src={sakuraLg} alt="sakura-large" />
+                </center>
+                <figcaption
+                style={{
+                  marginLeft:"20px",
+                }}
+                >
+                    <div
+                    style={{
+                      transform:"translateY(200px)"
+                    }}
+                    >
+                    <h2 style={{
+                      color:"white"
+                    }}>Sakura</h2>
+                      <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>Rp. 55.0000</h3>
+                     <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>250 ml</h3>
+                    </div>
+                   
+                </figcaption>
+              </figure>
+            </div>
+          
+            <div
+            style={{
+              backgroundColor:"#AE8DBA"
+            }}
+            className='product-card'
+            >     
+              <figure>
+                <center>
+                  <img src={lavenderLg} alt="lavender-large" />
+                </center>
+                <figcaption
+                style={{
+                  marginLeft:"20px",
+                }}
+                >
+                    <div
+                    style={{
+                      transform:"translateY(200px)"
+                    }}
+                    >
+                    <h2 style={{
+                      color:"white"
+                    }}>Lavender</h2>
+                      <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>Rp. 55.0000</h3>
+                     <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>250 ml</h3>
+                    </div>
+                   
+                </figcaption>
+              </figure>
+            </div>
+            <div
+            style={{
+              backgroundColor:"#B8D5A9"
+            }}
+            className='product-card'
+            >     
+              <figure>
+                <center>
+                  <img src={citrusSm} alt="jeruk-small" />
+                </center>
+                <figcaption
+                style={{
+                  marginLeft:"20px",
+                }}
+                >
+                    <div
+                    style={{
+                      transform:"translateY(200px)"
+                    }}
+                    >
+                    <h2 style={{
+                      color:"white"
+                    }}>Jeruk</h2>
+                      <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>Rp. 22.0000</h3>
+                     <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>30 ml</h3>
+                    </div>
+                   
+                </figcaption>
+              </figure>
+            </div>
+            <div
+            style={{
+              backgroundColor:"#DEB1B8"
+            }}
+            className='product-card'
+            >     
+              <figure>
+                <center>
+                  <img src={sakuraSm} alt="sakura-small" />
+                </center>
+                <figcaption
+                style={{
+                  marginLeft:"20px",
+                }}
+                >
+                    <div
+                    style={{
+                      transform:"translateY(200px)"
+                    }}
+                    >
+                    <h2 style={{
+                      color:"white"
+                    }}>Sakura</h2>
+                      <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>Rp. 22.0000</h3>
+                     <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>30 ml</h3>
+                    </div>
+                   
+                </figcaption>
+              </figure>
+            </div>
+            <div
+            style={{
+              backgroundColor:"#AE8DBA"
+            }}
+            className='product-card'
+            >     
+              <figure>
+                <center>
+                  <img src={lavenderSm} alt="lavender-small" />
+                </center>
+                <figcaption
+                style={{
+                  marginLeft:"20px",
+                }}
+                >
+                    <div
+                    style={{
+                      transform:"translateY(200px)"
+                    }}
+                    >
+                    <h2 style={{
+                      color:"white"
+                    }}>Lavender</h2>
+                      <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>Rp. 22.0000</h3>
+                     <h3 style={{
+                      color:"white",
+                      fontWeight:"lighter"
+                    }}>30 ml</h3>
+                    </div>
+                   
+                </figcaption>
+              </figure>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-const AnimatedCardAdv: React.FC<{ imgSrc: string, text: string }> = ({ imgSrc, text }) => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
-
-  const animationProps = useSpring({
-    opacity: inView ? 1 : 0,
-    transform: inView ? 'translateY(0)' : 'translateY(-50px)',
-    config: { duration: 700 },
-  });
-
-  return (
-    <animated.div ref={ref} className='card-adv' style={{ ...animationProps, backgroundColor: "white", padding: "10px", color: "#537343", fontWeight: "bold", letterSpacing: "2px" }}>
-      <center>
-        <img src={imgSrc} width={120} alt="kelebihan" />
-      </center>
-      <p style={{width:"240px",textAlign:"center"}}>{text}</p>
-    </animated.div>
-  );
-}
 
 function useSlideInAnimation(duration: number): SpringValues<{ opacity: number; transform: string }> {
   return useSpring({
@@ -194,15 +471,5 @@ function useSlideInAnimation(duration: number): SpringValues<{ opacity: number; 
   });
 }
 
-// function useSlideInFromTopAnimation(duration: number): SpringValues<{ opacity: number; transform: string }> {
-//   return useSpring({
-//     from: { opacity: 0, transform: 'translateY(-50px)' },
-//     to: { opacity: 1, transform: 'translateY(0)' },
-//     config: {
-//       duration,
-//       easing: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t)
-//     }
-//   });
-// }
 
 export default App;
