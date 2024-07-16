7131002d0bdc3b6655ba0f40768e113486fbb1d6
trinantaasembiring <kurniawan.rizki.2305356@students.um.ac.id>
2024-07-16 21:37:07 +0700

    Adding new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSpring, animated, SpringValues, useTransition, to } from '@react-spring/web';
 import dermaWashLogo from './assets/img/logo-header.png';
 import dermaWash from './assets/img/dermawash.png';
